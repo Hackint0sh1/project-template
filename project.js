97176bbb04b41530d7273c6a19ab8868374f2f52
EWB User <ewb@unassigned-hostname>
2014-11-11 17:47:33 -0500

    Kind of bounces

--- project.js
+++ project.js
@@ -1,46 +1,74 @@
 title('My Project');
 
 // put variables here, just like kahn academy
-var exampleVar = 100;
+
+var Xpos = 350
+var Ypos = 500
+var BallX = 391
+var BallY = 485
+var speed = 10
+var Top = 0
+var Left = 0
+var Right =  800
+var Bottom = 600
 
 // put functions here, just like kahn academy
-var exampleFunction = function() {
-    
-    // change color depending on key pressed
-    if (keyIsPressed && keyCode === SPACE) {
-        fill(255, 0, 0);
-    } else {
-        fill(0, 0, 255);
-    }
 
-    rect(300, 200, 55, 55);
-};
+    
+  
+
 
 // this code is executed once when the program is started
-var setup = function() {
 
-    // set up the size of the canvas (you probably don't want to change this!)
+var setup = function(){
+    
     size(800, 600);
 
 };
 
 // override draw function, by default it will be called 60 times per second
+
 var draw = function() {
+    background(255, 0, 0);
 
-    // clear the screen
-    background(255, 255, 255);
+    //paddle
+    fill(0, 0, 0);
+    rect(Xpos, Ypos, 80, 15);
+    if (keyIsPressed && keyCode === RIGHT) {
+            
+            Xpos = Xpos + 9;  
+    }
 
-    // some drawing commands; feel free to change these
-    fill(255, 0, 0);
+    if (keyIsPressed && keyCode === LEFT) {
+            Xpos = Xpos - 9;
+    }
 
-    // draw some text
-    textSize(50);
-    text("Hello, World!", exampleVar, exampleVar);
+    //ball
+   fill(4, 90, 222);
+   ellipse(BallY, BallY, 20, 20);
 
-    textSize(20);
-    text("Press the space bar...", 500, 500);
+   BallY = BallY - speed; 
 
-    // call a function (defined up above)
-    exampleFunction();
+   if (BallY < 0) {
+       speed = -10;
+    }
 
 };
+
+
+
+
+
+
+
+
+
+    // some drawing commands; feel free to change these
+    
+
+    // draw some text
+ 
+
+    // call a function (defined up above)
+    
+
