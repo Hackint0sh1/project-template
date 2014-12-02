6d9b0f1055a8b18d7206d47226b06b3e4517c197
EWB User <ewb@unassigned-hostname>
2014-12-02 17:41:37 -0500

    Bounces off paddle and death message

--- project.js
+++ project.js
@@ -1,29 +1,30 @@
 title('LENNY');
 // put variables here, just like kahn academy
-var x = 391
-var y = 485
-var speed = 5
-var ySpeed = 5
-var boundL = 0
-var boundR = 800
-var boundT = 0
-var boundB = 600
-var xSize = 80
-var ySize = 15
+var x = 391;
+var y = 485;
+var speed = 5;
+var ySpeed = 5;
+var boundL = 0;
+var boundR = 800;
+var boundT = 0;
+var boundB = 600;
+var xSize = 80;
+var ySize = 15;
 ///////////////////
-var Xpos = 350
-var Ypos = 500
-var BallX = 391
-var BallY = 485
-var Top = 0
-var Left = 0
-var Right =  800
-var Bottom = 600
-
+var Xpos = 350;
+var Ypos = 500;
+var BallX = 391;
+var BallY = 485;
+var Top = 0;
+var Left = 0;
+var Right =  800;
+var Bottom = 600;
+var dClicker = 0;
+var brickSize = 54;
 // put functions here, just like kahn academy
 
     
-  
+var i = [
 
 
 // this code is executed once when the program is started
@@ -55,6 +56,9 @@ var draw = function() {
     
     x = x + speed;
     y = y + ySpeed;
+   
+  
+
     
     if (x > boundR){
         speed = -2.5 - Math.random() * 2.5;
@@ -64,57 +68,26 @@ var draw = function() {
         speed = 2.5 + Math.random() * 2.5;
     }
     
-    if (y > boundB) {
-        ySpeed = -2.5 - Math.random() * 2.5;
+    if (y + 13 >= Ypos && x >= Xpos && x <= (Xpos+50)) {
+	ySpeed = -2 - Math.random() * 2.5;
+	dClicker = 1;
+    }else if(y > Ypos+50){
+        ySpeed = 0;
+        speed = 0;
+        
+        Xpos = 0;
+        background(0, 21, 255);
+	if (dClicker === 1) {
+	    dClicker = 0;
+	    alert("YOU DIED");
+};
+        
     }
-    
     if (y < boundT) {
         ySpeed = 2.5 + Math.random() * 2.5;
     }
 };
-/*   //ball
-   fill(4, 90, 222);
-   ellipse(BallY, BallY, 20, 20);
 
-   BallY = BallY - speed; 
-
-   if (BallY < 0) {
-       speed = -5;
-    }
-
-    if (BallY > Top) {
-	BallX = +5;
-     }
-    if (BallY > Ypos) {
-	speed = +5;
-     }
-    if (BallX > Xpos) {
-	speed = -5;
-     }
-    
-
-};
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // some drawing commands; feel free to change these
-    
-
-    // draw some text
- 
-
-    // call a function (defined up above)
+   
     
 
